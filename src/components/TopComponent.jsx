7e61c129b1e29832f1dd4db9ee59f7cf98987e28
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-04-25 09:25:11 +0200

    I made a card

--- src/components/TopComponent.jsx
+++ src/components/TopComponent.jsx
@@ -1,13 +1,10 @@
 
 import "../styles/TopComponent.css";
-import Card from "./Card";
 
 const TopComponent = () => {
   return (
    <section className="headwrapper">
      <h1 className="headline">E-Wallet</h1>
-
-     <Card />
    </section>
   )
 }
