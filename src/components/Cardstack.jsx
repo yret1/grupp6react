6dc2221c9ac9c7f1a513be9020c922ed456d7c8a
vickanS <stennvictoria@gmail.com>
2024-04-25 18:06:56 +0200

    har lagt till info i korten samt styling. Enklare styling av knappen.

--- src/components/Cardstack.jsx
+++ src/components/Cardstack.jsx
@@ -3,9 +3,41 @@ import '../Styles/Cardstack.css'
 const Cardstack = () => {
   return (
   <section className='theStack'>
-    <div className='blackCard'></div>
-    <div className='purpleCard'></div>
-    <div className='redCard'></div>
+    <div className='blackCard'>
+        <section className='blackCardsection'>
+            <div className='blackCardicon1'>ikon 1</div>
+            <div className='blackCardicon2'>ikon 2</div>
+        </section>
+    </div>
+    <div className='purpleCard'>
+    <section className='purpleCardsection'>
+            <div className='purpleCardicon1'>ikon 1</div>
+            <div className='purpleCardicon2'>ikon 2</div>
+        </section>
+    </div>
+    <div className='redCard'>
+    <section className='redCardicons'>
+            <div className='redCardicon1'>ikon 1</div>
+            <div className='redCardicon2'>ikon 2</div>
+        </section>
+        <section redCardnr>
+          <span className='rednr'>6666</span>
+          <span className='rednr'>6666</span>
+          <span className='rednr'>6666</span>
+          <span className='rednr'>6666</span>
+          <span className='rednr'>6666</span>
+        </section>
+        <section className='rcardInfo'>
+          <section className='rCardholder'>
+        <div className='rcardholderName'>CARDHOLDER NAME</div>
+        <div className='rName'>RACHEL RICHTER</div>
+        </section>
+        <section className='rValid'>
+        <div className='rvalidThru'>VALID THRU</div>
+        <div className='rvalidDate'>10/24</div>
+        </section>
+        </section>
+    </div>
   </section>
   )
 }
--- src/components/Yellowcard.jsx
+++ src/components/Yellowcard.jsx
@@ -5,7 +5,27 @@ const Yellowcard = () => {
   return (
     <section className='cardwrapper'>
         <section className='ycard'>
-            <div></div>
+        <section className='yellowCardicons'>
+            <div className='yellowCardicon1'>ikon 1</div>
+            <div className='yellowCardicon2'>ikon 2</div>
+        </section>
+        <section yellowCardnr>
+          <span className='yellownr'>1234</span>
+          <span className='yellownr'>5678</span>
+          <span className='yellownr'>5678</span>
+          <span className='yellownr'>9101</span>
+          <span className='yellownr'>1123</span>
+        </section>
+        <section className='ycardInfo'>
+          <section className='yCardholder'>
+        <div className='ycardholderName'>CARDHOLDER NAME</div>
+        <div className='yName'>RACHEL RICHTER</div>
+        </section>
+        <section className='yValid'>
+        <div className='yvalidThru'>VALID THRU</div>
+        <div className='yvalidDate'>12/24</div>
+        </section>
+        </section>
         </section>
     </section>
   )
